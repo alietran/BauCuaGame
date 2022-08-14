09bededaf30b5a1c5607ed63b4c47d45e516534e
Ngọc Diệp <61446841+alietran@users.noreply.github.com>
2022-08-14 17:14:59 +0700

    build

---
+++ build/precache-manifest.7211892233e8360519514224dfc874e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fae0920f1abe7639e6f57daeadb296",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b54ca8e680a65291b8c5",
+    "url": "/static/css/main.bbb36482.chunk.css"
+  },
+  {
+    "revision": "4cd3f699665114188f95",
+    "url": "/static/js/2.a69b9e59.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.a69b9e59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b54ca8e680a65291b8c5",
+    "url": "/static/js/main.3b9e1407.chunk.js"
+  },
+  {
+    "revision": "ef34d64ce30fc410a6f3",
+    "url": "/static/js/runtime-main.3a145b39.js"
+  },
+  {
+    "revision": "1716f3f87bf9af14a763249e77f202da",
+    "url": "/static/media/Pony.1716f3f8.ttf"
+  }
+]);
